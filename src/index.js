23f34f72a3de9b95c7ac07e672327f8a33c1740c
golam71 <74001687+golam71@users.noreply.github.com>
2025-04-27 21:06:07 +0600

    :zap: Refactor Player constructor: remove constructor function and replace with object prototype access

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-function Player(name, marker) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-  this.name = name;
-  this.marker = marker;
-  this.sayName = function () {
-    console.log(this.name);
-  };
-}
+let x = {};
 
-let x =new Player("golam","x")
+Object.getPrototypeOf(x)
