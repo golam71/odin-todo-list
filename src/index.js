05d6d207b841faec827241818fea652065273a81
golam71 <74001687+golam71@users.noreply.github.com>
2025-04-28 23:14:12 +0600

    🎨 Format code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-let x = {};
+class Golam {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  golam() {
+    return `${this.name} is ${String(this.age)} years olda`;
+  }
+}
 
-Object.getPrototypeOf(x)
+class Homie extends Golam {
+  constructor(name, age) {
+    super(name, age);
+  }
+  g() {
+    return super.golam();
+  }
+}
+
+let x = new Homie("golam", 19);
+
+console.log(x.g());
