80cd2f232f2b605e993f7b42078cdf2da8c1cf1c
golam71 <74001687+golam71@users.noreply.github.com>
2025-05-05 22:34:39 +0600

    :tada: Refactor edit and delete button functionality: modularize initialization, enhance dialog handling, and streamline todo updates

--- src/scripts/edit-delete.js
+++ src/scripts/edit-delete.js
@@ -1,29 +1,89 @@
 import { saveData } from "./save.js";
 import { openDialog } from "./dialog.js";
+import { addTodoToDOM } from "./todo.js";
 
-document.querySelectorAll("#todo-delete").forEach((deleteButton) => {
-  deleteButton.onclick = (e) => {
-    let todo = e.target.closest("#todo");
-    todo.remove();
-    saveData();
+let editingTodo = null;
+
+export function initializeDeleteButtons() {
+  document.querySelectorAll("#todo-delete").forEach((deleteButton) => {
+    deleteButton.onclick = (e) => {
+      let todo = e.target.closest("#todo");
+      todo.remove();
+      saveData();
+    };
+  });
+}
+
+export function initializeEditButtons() {
+  document.querySelectorAll("#todo-edit").forEach((editButton) => {
+    editButton.onclick = (e) => {
+      editingTodo = e.target.closest("#todo");
+
+      let title = editingTodo.querySelector(".todo-title").innerText;
+      let description =
+        editingTodo.querySelector(".todo-description").innerText;
+      let date = editingTodo.querySelector(".todo-date").innerText;
+      let priority = editingTodo.querySelector(".todo-priority").innerText;
+
+      document.getElementById("title").value = title;
+      document.getElementById("description").value = description;
+      document.getElementById("due-date").value = date;
+      document.getElementById("priority").value = priority;
+
+      openDialog();
+    };
+  });
+}
+
+initializeDeleteButtons();
+initializeEditButtons();
+
+function submitDialogData() {
+  return {
+    title: document.getElementById("title").value,
+    description: document.getElementById("description").value,
+    dueDate: document.getElementById("due-date").value,
+    priority: document.getElementById("priority").value,
   };
-});
+}
 
-document.querySelectorAll("#todo-edit").forEach((editButton) => {
-  editButton.onclick = (e) => {
-    let todo = e.target.closest("#todo");
-    let title = todo.querySelector(".todo-title").innerText;
-    let description = todo.querySelector(".todo-description").innerText;
-    let date = todo.querySelector(".todo-date").innerText;
-    let priority = todo.querySelector(".todo-priority").innerText;
+document.getElementById("submit").onclick = () => {
+  const data = submitDialogData();
 
-    document.getElementById("title").value = title;
-    document.getElementById("description").value = description;
-    document.getElementById("due-date").value = date;
-    document.getElementById("priority").value = priority;
+  if (editingTodo) {
+    editingTodo.querySelector(".todo-title").innerText = data.title;
+    editingTodo.querySelector(".todo-description").innerText = data.description;
+    editingTodo.querySelector(".todo-date").innerText = data.dueDate;
+    editingTodo.querySelector(".todo-priority").innerText = data.priority;
+    editingTodo = null;
+  } else {
+    addTodoToDOM(data);
+  }
 
-    document.getElementById("submit").
-    openDialog();
-    // saveData();
-  };
-});
+  saveData();
+  resetDialog();
+  closeDialog();
+};
+
+document.getElementById("close").onclick = () => {
+  closeDialog();
+  resetDialog();
+};
+
+document.getElementById("add-todo").onclick = () => {
+  editingTodo = null;
+  resetDialog();
+  openDialog();
+};
+
+function closeDialog() {
+  document.getElementsByClassName("todo-container")[0].style.display = "block";
+  document.querySelector("dialog").close();
+}
+
+function resetDialog() {
+  document.getElementById("title").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("due-date").value = "";
+  document.getElementById("priority").value = "Low";
+}
